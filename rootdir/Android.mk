ed22871c2df915b1bd49602ccb40515649a8aaaf
Gabriel Gomez <g.a.gomez@gmail.com>
2018-06-23 22:07:08 -0300

    pearlyn: link persist.service.adb.enable with Razer's "device mode"
    This will switch the usb port into "debug mode" when adb is enabled/disabled
    
    Signed-off-by: Gabriel Gomez <g.a.gomez@gmail.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -97,14 +97,6 @@ LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.razer.peripherals.sh
-LOCAL_MODULE_TAGS  := optional eng
-LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.razer.peripherals.sh
-LOCAL_MODULE_PATH       := $(TARGET_ROOT_OUT)
-include $(BUILD_PREBUILT)
-
-include $(CLEAR_VARS)
 LOCAL_MODULE       := init.razer.rc
 LOCAL_MODULE_TAGS  := optional eng
 LOCAL_MODULE_CLASS := ETC
