93de285e954c7d00aee16714fd4f769fb3879c12
gabrielgagz <g.a.gomez@gmail.com>
2018-08-31 19:54:22 -0300

    pearlyn: use legacy power HAL
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- treble.mk
+++ treble.mk
@@ -47,7 +47,7 @@ PRODUCT_PACKAGES += \
 
 # Power HAL
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.0-service-qti
+    android.hardware.power@1.0-service-impl
 
 # RenderScript HAL
 PRODUCT_PACKAGES += \
