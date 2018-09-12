33d4fc404a2705be39760c2e197f8919b69d28d1
gabrielgagz <g.a.gomez@gmail.com>
2018-09-11 21:03:29 -0300

    pearlyn: add compatibility_matrix.xml
    From android_device_samsung_apq8084-common
    
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -147,6 +147,7 @@ TARGET_USES_64_BIT_BINDER := true
 
 # HIDL
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/compatibility_matrix.xml
 
 # TWRP (optional)
 ifeq ($(WITH_TWRP),true)
