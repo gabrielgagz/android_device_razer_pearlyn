2805a3592a7d784d94cd3d3715b91415d08bf84a
gabrielgagz <g.a.gomez@gmail.com>
2018-09-23 13:21:49 -0300

    pearlyn: treble.mk: build tv.cec and tv.input
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- treble.mk
+++ treble.mk
@@ -61,6 +61,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.thermal@1.0-impl \
     android.hardware.thermal@1.0-service    
+    
+# TV
+PRODUCT_PACKAGES += \
+    android.hardware.tv.cec@1.0-impl \
+    android.hardware.tv.input@1.0-impl       
 
 # USB
 PRODUCT_PACKAGES += \
