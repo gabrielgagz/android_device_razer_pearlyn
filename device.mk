e8f981f987231b4edf85bcf5959691fc9798f013
gabrielgagz <g.a.gomez@gmail.com>
2018-11-18 03:31:32 -0300

    pearlyn: remove unused sysinit scripts
    Signed-off-by: gabrielgagz <g.a.gomez@gmail.com>

--- device.mk
+++ device.mk
@@ -74,23 +74,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_tv.xml:system/etc/media_codecs_google_tv.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
     
-# Sys Init
-PRODUCT_PACKAGES += \
-    hcidump.sh \
-    hsic.control.bt.sh \
-    init.ath3k.bt.sh \
-    init.crda.sh \
-    init.qcom.audio.sh \
-    init.qcom.coex.sh \
-    init.qcom.efs.sync.sh \
-    init.qcom.fm.sh \
-    init.qcom.modem_links.sh \
-    init.qcom.post_boot.sh \
-    init.qcom.sdio.sh \
-    init.qcom.wifi.sh \
-    qca6234-service.sh \
-    usf_post_boot.sh
-    
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/sec_config:$(TARGET_COPY_OUT_VENDOR)/etc/sec_config        
     
