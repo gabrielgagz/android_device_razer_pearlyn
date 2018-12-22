bdea9f0b84476e2a468da7702d54ced1c96a8f6b
Bruno Martins <bgcngm@gmail.com>
2018-12-22 18:58:43 +0000

    pearlyn: enable memory optimizations
     * Enable B-services aging propagation and delay service restart
       for devices with 2GB or less memory.
    
    Change-Id: I81a09cc8425602bfc586875b7d36a4f56a226908

--- device.mk
+++ device.mk
@@ -118,6 +118,11 @@ PRODUCT_PROPERTY_OVERRIDES += \
 	ro.product.first_api_level=22 \
 	ro.com.google.clientidbase=android-pearlyn \
 	ro.sf.lcd_density=320
+	
+# Memory optimizations
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.vendor.qti.am.reschedule_service=true \
+    ro.vendor.qti.sys.fw.bservice_enable=true	
     
 # Audio
 PRODUCT_PACKAGES += \
